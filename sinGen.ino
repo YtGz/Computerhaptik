847b88c0f93e3be708eb20ad270387613165adb5
csaq6030 <chhong.lee@student.uibk.ac.at>
2017-03-17 11:39:48 +0100

    Add files via upload

---
+++ sinGen.ino
@@ -0,0 +1,22 @@
+const float twoPi = 6.28;
+//sample rate -> samples per cycle/period
+const byte sampleRate = 50;
+byte current = 0;
+double result = 0.0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  if(current >=50){
+    current = 0;
+  }else{
+    current++;
+  }
+  result = sin(current*twoPi/sampleRate);
+}
+
+
