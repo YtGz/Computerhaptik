0c8e3cfde7923397099dcb566609a40803067279
csaq6030 <chhong.lee@student.uibk.ac.at>
2017-03-24 13:16:01 +0100

    Update measurement.R

--- measurement/measurement.R
+++ measurement/measurement.R
@@ -1,9 +1,20 @@
 deg = c(-50,-40,-30,-20,-10,0,10,20,30,40,50)
 count = c(-3281,-2684,-1743,-1171,-719,152,808,1145,2119,2817,3728)
 
-linReg = lm(count ~ deg)
+countDeg = lm(count ~ deg)
+degCount = lm(deg~count)
 
-summary(linReg)
+
+
+summary(countDeg)
+summary(degCount)
 
 plot(deg, count, pch = 19)
-abline(linReg, col = "red")
+abline(countDeg, col = "red")
+
+
+plot(count, deg, pch = 19)
+abline(degCount, col = "red")
+
+predict(degCount, data.frame(count=2000))
+
