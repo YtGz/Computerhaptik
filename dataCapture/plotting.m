809942d04058ec9a5403d9a112333abe1d83815a
YtGz <Philipp.Wirtenberger@student.uibk.ac.at>
2017-04-18 18:20:53 +0200

    clean up data files

--- dataCapture/plotting.m
+++ dataCapture/plotting.m
@@ -1,8 +1,8 @@
-load data.txt;
-[n,p] = size(data);
+load Spring_k0_03.txt;
+[n,p] = size(Spring_k0_03);
 t = 1:n;
 t = t*0.0005;
-data(:,2) = data(:,2)/1000;
-plot(t,data(:,2)),
+Spring_k0_03(:,2) = Spring_k0_03(:,2)*10;
+plot(t,Spring_k0_03),
 xlabel('Time'), ylabel('Force')
 title('Spring');
